580758471369bd2a48199b8a944cb5965cd33a33
sharbel0022 <49398736+sharbel0022@users.noreply.github.com>
2023-03-30 14:54:55 +0200

    Add files via upload

--- cms2/b/index.php
+++ cms2/b/index.php
@@ -1,13 +1,42 @@
+<?php
+require "connect.php";
+$ID= $_GET['id'];
+$query = $pdo->prepare("select * from posts where ID = $ID");
+$query->execute();
+while($posts = $query->fetch()){ 
+$headline= $posts[1];
+$text= $posts[2];
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Main Page</title>
+  <style>
+h1{
+  color:red;
+}
+
+
+
+
+
+
+
+
+
+
+  </style>
 </head>
 <body>
-  <h1>Welcome to the Main Page</h1>
-  <p>Click the button below to go to the admin page:</p>
-  <form action="admin.php" method="post">
-  <button type="submit">Admin Page</button>
-</form>
+  <?php $query = $pdo->prepare("select * from posts ");
+$query->execute();
+while($posts = $query->fetch()){ 
+echo '<a href="index.php?id='.$posts[0].'">'.$posts[1].'</a><br>';
+}   ?>
+
+  <h1><?php echo  $headline; ?> </h1>
+  <p><?php echo $text; ?> </p>
+
 </body>
 </html>
